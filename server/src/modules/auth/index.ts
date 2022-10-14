ce0892afc5fcafaa662fc3de4434796724c174f1
Roman <4elove4ek40@gmail.com>
2022-10-14 13:26:14 +0600

    refactor(@lm-server): auth module

--- server/src/modules/auth/index.ts
+++ server/src/modules/auth/index.ts
@@ -1,1 +1,6 @@
-export * from './routes';
+import type { FastifyInstance } from 'fastify';
+import { localAuth } from './local-auth';
+
+export const authRoutes = async (server: FastifyInstance) => {
+  await server.register(localAuth, { prefix: '/local' });
+};
--- server/src/modules/auth/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import type { FastifyInstance } from 'fastify';
-import { localAuth } from './local-auth';
-
-export const authRoutes = async (server: FastifyInstance) => {
-  await server.register(localAuth, { prefix: '/local' });
-};
